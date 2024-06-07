ca87953f2c110caa10ea872194c0da177f1b5e34
Jacky Yang <jackyyang1670@gmail.com>
2024-06-06 13:32:25 -1100

    Added kyu files

---
+++ 7 kyu/Reverse words.js
@@ -0,0 +1,10 @@
+/* DESCRIPTION:
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps" */
+
+function reverseWords(str) {
+    return str.split(' ').map(str => [...str].reverse().join('')).join(' ');
+  }
