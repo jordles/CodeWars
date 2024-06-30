8686e745d2f516cc1c850841f90a0274bd2c2201
Jacky Yang <jackyyang1670@gmail.com>
2024-06-29 18:00:43 -1100

    Added kyu files

---
+++ 8 kyu/Opposite number.js
@@ -0,0 +1,12 @@
+/* DESCRIPTION:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 */
+
+//SOLUTION
+
+const opposite = number => -number;
