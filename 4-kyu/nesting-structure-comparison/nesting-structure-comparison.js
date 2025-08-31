0c77c70d5cc2691bec52bf0e4064cf6e8550bd8e
Jacky Yang <jackyyang1670@gmail.com>
2025-08-30 23:43:47 -0700

    Add solution - CodeHub

---
+++ 4-kyu/nesting-structure-comparison/nesting-structure-comparison.js
@@ -0,0 +1,9 @@
+Array.prototype.sameStructureAs = function (other) {
+  if (!Array.isArray(other) || this.length !== other.length) return false;
+​
+  return this.every((el, i) =>
+    Array.isArray(el)
+      ? el.sameStructureAs(other[i])
+      : !Array.isArray(other[i])
+  );
+};
